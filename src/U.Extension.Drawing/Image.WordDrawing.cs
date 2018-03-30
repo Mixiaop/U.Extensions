372e269e1ccb44e5916030817849b6f6b2f6b48b
Mixiaop <112949824@qq.com>
2018-03-30 13:55:15 +0800

    1

---
+++ src/U.Extension.Drawing/Image.WordDrawing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+public static partial class Extensions
+{
+    public const string DefaultFontFamily = "Microsoft YaHei";
+    public static void WordDrawing(this Image img, string text, Point point, string saveFilePath, Brush brush = null, Font font = null)
+    {
+        if (saveFilePath.IsNullOrEmpty()) {
+            throw new Exception("Please enter saveFilePath");
+        }
+
+        if (brush == null)
+            brush = new SolidBrush(Color.Black);
+
+
+        if (font == null)
+            font = new Font(DefaultFontFamily, 20);
+
+        using (Graphics g = Graphics.FromImage(img)) {
+            g.DrawString(text, font, brush, point);
+        }
+
+        img.Save(saveFilePath, ImageFormat.Gif);
+        img.Dispose();
+    }
+
+    public static void WordDrawing(this Image img, string text, Point point, string saveFilePath, int fontSize, bool isFontBold = false) {
+        var font = new Font(DefaultFontFamily, fontSize, (isFontBold ? FontStyle.Bold : FontStyle.Regular));
+        
+        img.WordDrawing(text, point, saveFilePath, null, font);
+    }
+}
---
+++ src/U.Extension.Tests/Drawing/ImageWordDrawing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace U.Extension.Tests.Drawing
+{
+    [TestClass]
+    public class ImageWordDrawing
+    {
+
+        [TestMethod]
+        public void DrawingTests()
+        {
+            var path = @"D:\!Github\U.Extensions\src\U.Extension.Tests\Drawing\test.jpg";
+            var path2 = @"D:\!Github\U.Extensions\src\U.Extension.Tests\Drawing\test2.jpg";
+            Image img = Image.FromFile(path);
+            img.WordDrawing("你好", new Point(100, 150), path2, 30, true);
+        }
+    }
+}
